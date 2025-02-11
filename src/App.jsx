018042372a3e9c21b84fc581865b6f74d515f1be
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-11 18:25:57 +0100

    add side menu for smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import "./App.css";
 import SideMenu from "./components/SideMenu";
 import Icon from "./assets/bank-dash-icon.svg";
 import IconText from "./assets/bank-dash-text.svg";
+import NotificationsIcon from "./assets/notifications-icon.svg";
+import SettingsIcon from "./assets/settings-icon.svg";
+import OptionsButton from "./assets/options-button.svg?react";
+import MagnifyingGlass from "./assets/magnifying-glass.svg";
+import ProfilePicture from "./assets/profile-picture.svg";
 
 import { Outlet } from "react-router-dom";
 
+import { useState } from "react";
+
 function App() {
+  const [displaySideMenu, setDisplaySideMenu] = useState(false);
+
   return (
     <>
       <div>
@@ -24,8 +33,64 @@ function App() {
             }
           }
         >
-          <div style={{ marginTop: 106 }}>
-            <Outlet />
+          <div id="header-container">
+            <div
+              className="center-content"
+              style={{
+                width: "100%",
+                paddingLeft: 24,
+                paddingRight: 30,
+                justifyContent: "space-between",
+              }}
+            >
+              <button
+                onClick={() => {
+                  setDisplaySideMenu(!displaySideMenu);
+                }}
+                className="options-button"
+              >
+                <OptionsButton />
+              </button>
+              <h1 className="overview">Overview</h1>
+              <div className=" top-right-options ">
+                <div className="center-content">
+                  <form>
+                    <div className="search-field">
+                      <button className="round-button" aria-label="search">
+                        <img src={MagnifyingGlass} alt="magnifying-glass" />
+                      </button>
+                      <input className="search-bar" placeholder="Search ..." />
+                    </div>
+                  </form>
+                  <div className="round-button" style={{ flexShrink: 1 }}>
+                    <img src={SettingsIcon} alt="settings-icon" />
+                  </div>
+                  <div className="round-button">
+                    <img src={NotificationsIcon} alt="notifications-icon" />
+                  </div>
+                  <img
+                    src={ProfilePicture}
+                    alt="profile-picture"
+                    style={{ width: 45 }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            {displaySideMenu ? (
+              <div className="side-menu-container">
+                <SideMenu
+                  toggleDisplaySidMenu={() => setDisplaySideMenu(false)}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
+            <div style={{ marginTop: 106 }}>
+              <Outlet />
+            </div>
           </div>
         </div>
       </div>
