fdd4b041ab69b777879afdd3bb189e2cec26cc40
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-11 19:15:58 +0100

    add credit cards to transactions screen

--- src/screens/TransactionsScreen.jsx
+++ src/screens/TransactionsScreen.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import CreditCard from "../components/CreditCard.jsx";
 
 export default function TransactionsScreen() {
-  return <div>TransactionsScreen</div>;
+  return (
+    <>
+      <div className="wrapper">
+        <div className="box my-cards">
+          <div className="top-banner">
+            <h2>My Cards</h2>
+            <h3>+ Add Card</h3>
+          </div>
+          <div className="cards">
+            <CreditCard theme="dark-blue" />
+            <CreditCard theme="light" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
