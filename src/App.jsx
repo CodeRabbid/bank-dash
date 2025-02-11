fcc2e2329b676527ab899c35bb2ba13e5ea53e44
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-11 18:03:24 +0100

    display side menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import SideMenu from "./components/SideMenu";
+import Icon from "./assets/bank-dash-icon.svg";
+import IconText from "./assets/bank-dash-text.svg";
 
 import { Outlet } from "react-router-dom";
 
@@ -6,8 +9,24 @@ function App() {
   return (
     <>
       <div>
-        <div style={{ marginTop: 106 }}>
-          <Outlet />
+        <div id="left-side">
+          <div id="logo-container">
+            <img src={Icon} alt="icon" id="icon" />
+            <img src={IconText} alt="bank-dash" id="icon-text" />
+          </div>
+          <SideMenu toggleDisplaySidMenu={() => setDisplaySideMenu(false)} />
+        </div>
+        <div
+          id="right-side"
+          style={
+            {
+              // opacity: 0.5,
+            }
+          }
+        >
+          <div style={{ marginTop: 106 }}>
+            <Outlet />
+          </div>
         </div>
       </div>
     </>
