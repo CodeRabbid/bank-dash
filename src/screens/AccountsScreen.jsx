ec648ba46da3f234691934add068fb345eb182de
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-11 19:32:01 +0100

    add four small sections to account screen

--- src/screens/AccountsScreen.jsx
+++ src/screens/AccountsScreen.jsx
@@ -1,5 +1,56 @@
+import "./AccountsScreen.css";
 import React from "react";
+import BalanceIcon from "../assets/balance-icon.svg";
+import SavingIcon from "../assets/saving-icon.svg";
+import IncomeIcon from "../assets/income-icon.svg";
+import ExpenseIcon from "../assets/expense-icon.svg";
+
+const USDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+  minimumFractionDigits: 0,
+});
 
 export default function AccountsScreen() {
-  return <div>AccountsScreen</div>;
+  return (
+    <>
+      <div className="wrapper">
+        <div className="box four-small-sections">
+          <div className="two-small-sections">
+            <SmallSection
+              title={"My Balance"}
+              amount={5600}
+              Icon={BalanceIcon}
+            />
+            <SmallSection title={"Income"} amount={3460} Icon={IncomeIcon} />
+          </div>
+          <div className="box two-small-sections">
+            <SmallSection title={"Expense"} amount={7920} Icon={ExpenseIcon} />
+            <SmallSection
+              title={"Total Saving"}
+              amount={12500}
+              Icon={SavingIcon}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+const SmallSection = ({ title, amount, Icon }) => {
+  return (
+    <>
+      <div className="small-section-frame">
+        <div>
+          <img src={Icon} width={60} />
+        </div>
+        <div style={{ padding: 10 }}>
+          <div className="small-section-title">{title}</div>
+          <div className="small-section-amount">{USDollar.format(amount)}</div>
+        </div>
+      </div>
+    </>
+  );
+};
