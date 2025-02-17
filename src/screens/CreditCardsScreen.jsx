3ebee2feecb62be391a3fa9748b80a2826666c85
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-17 20:51:40 +0100

    add three credit cards to credit card screen

--- src/screens/CreditCardsScreen.jsx
+++ src/screens/CreditCardsScreen.jsx
@@ -1,5 +1,20 @@
+import "./CreditCardsScreen.css";
 import React from "react";
+import CreditCard from "../components/CreditCard";
 
 export default function CreditCardsScreen() {
-  return <div>CreditCardsScreen</div>;
+  return (
+    <div className="wrapper">
+      <div className="box three-cards">
+        <div className="top-banner">
+          <h2>My Cards</h2>
+        </div>
+        <div className="cards">
+          <CreditCard theme="light-blue" />
+          <CreditCard theme="dark-blue" />
+          <CreditCard theme="light" />
+        </div>
+      </div>
+    </div>
+  );
 }
