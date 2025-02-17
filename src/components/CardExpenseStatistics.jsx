11b206ae63161996c3489621f3f1ed125dd1ad31
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-17 21:44:28 +0100

    add card expnese statistics to Credit Card Screen

---
+++ src/components/CardExpenseStatistics.jsx
@@ -0,0 +1,54 @@
+import "./CardExpenseStatistics.css";
+import { PolarArea } from "react-chartjs-2";
+
+const data = {
+  labels: ["DBL Bank", "ABM Bank", "BRC Bank", "MCP Bank"],
+  datasets: [
+    {
+      label: " ",
+      data: [40, 35, 30, 25],
+      backgroundColor: ["#16DBCC", "#FF82AC", "#FFBB38", "#4C78FF"],
+      borderWidth: 0,
+      offset: 0,
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "bottom",
+      align: "middle",
+      labels: {
+        usePointStyle: true,
+        pointStyle: "circle",
+        color: "#718EBF",
+      },
+    },
+
+    tooltip: {
+      enabled: false,
+    },
+    datalabels: {
+      display: false,
+    },
+    r: {
+      ticks: {
+        display: false, // Remove vertical numbers
+      },
+      grid: {
+        display: false, // Removes the circulair lines
+      },
+    },
+  },
+};
+
+export default function CardExpenseStatistics() {
+  return (
+    <div className="frame">
+      <PolarArea options={options} data={data} />
+    </div>
+  );
+}
--- src/screens/CreditCardsScreen.jsx
+++ src/screens/CreditCardsScreen.jsx
@@ -1,11 +1,12 @@
 import "./CreditCardsScreen.css";
 import React from "react";
 import CreditCard from "../components/CreditCard";
+import CardExpenseStatistics from "../components/CardExpenseStatistics";
 
 export default function CreditCardsScreen() {
   return (
-    <div className="wrapper">
-      <div className="box three-cards">
+    <>
+      <div className="box three-cards-section">
         <div className="top-banner">
           <h2>My Cards</h2>
         </div>
@@ -15,6 +16,16 @@ export default function CreditCardsScreen() {
           <CreditCard theme="light" />
         </div>
       </div>
-    </div>
+      <div className="wrapper">
+        <div className="box card-expese-statistics-section">
+          <div className="top-banner">
+            <h2>Card Expense Statistics</h2>
+          </div>
+          <div className="card-expense-statistics">
+            <CardExpenseStatistics />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
