872a52a118f0381556dd4d8c184da86ce9aef1f8
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-19 21:18:38 +0100

    refactor class names in transaction tabs

--- src/components/TransactionsScreen/RecentTransactionsTabs.jsx
+++ src/components/TransactionsScreen/RecentTransactionsTabs.jsx
@@ -2,36 +2,29 @@ import "./RecentTransactionsTabs.css";
 
 export default function RecentTransactionsTabs({ tab, setTab }) {
   return (
-    <div className="recent-transactions-tab-frame">
+    <div className="recent-transactions-tabs">
       <button
-        className={
-          "recent-transactions-tab-button " + (tab === "all" ? "active" : "")
-        }
+        className={tab === "all" ? "active" : ""}
         onClick={() => setTab("all")}
         style={{ color: tab === "all" ? "#1814F3" : "#718EBF" }}
       >
         All Transactions
       </button>
       <button
-        className={
-          "recent-transactions-tab-button " + (tab === "income" ? "active" : "")
-        }
+        className={tab === "income" ? "active" : ""}
         onClick={() => setTab("income")}
         style={{ color: tab === "income" ? "#1814F3" : "#718EBF" }}
       >
         Income
       </button>
       <button
-        className={
-          "recent-transactions-tab-button " +
-          (tab === "expense" ? "active" : "")
-        }
+        className={tab === "expense" ? "active" : ""}
         onClick={() => setTab("expense")}
         style={{ color: tab === "expense" ? "#1814F3" : "#718EBF" }}
       >
         Expense
       </button>
-      <div className="recent-transactions-tab-indicator"></div>
+      <div className="indicator"></div>
     </div>
   );
 }
