8e0a2880881dccf6f29e0c695b08a6bbcf6bb182
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-19 19:33:21 +0100

    add security tab to settings screen

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -2,7 +2,7 @@ import "./Toggle.css";
 
 export default function Toggle({ id }) {
   return (
-    <div class="checkbox-container">
+    <div class="toggle checkbox-container">
       <input type="checkbox" id={id} className="toggle-input" />
       <label for="digital-currency" class="toggle"></label>
     </div>
--- src/screens/SecurityScreen.jsx
+++ src/screens/SecurityScreen.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import "./SecurityScreen.css";
+import Toggle from "../components/Toggle";
 
 export default function SecurityScreen() {
-  return <div>SecurityScreen</div>;
+  return (
+    <div className="security container">
+      <div className="title">Two-factor Authentication</div>
+      <div class="checkboxes-container">
+        <div className="toggle-container">
+          <Toggle id="digital-currency" />
+          <label for="digital-currency" class="label">
+            Enable two factor authentication
+          </label>
+        </div>
+      </div>
+      <div className="title">Change Password</div>
+      <div className="input-container">
+        <label htmlFor="">Current Password</label>
+        <div className="input-frame">
+          <input type="text" placeholder="**********" />
+        </div>
+      </div>
+      <div className="input-container">
+        <label htmlFor="">New Password</label>
+        <div className="input-frame">
+          <input type="text" placeholder="**********" />
+        </div>
+      </div>
+      <div className="submit-button-container">
+        <button>Save</button>
+      </div>
+    </div>
+  );
 }
