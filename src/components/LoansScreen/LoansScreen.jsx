81119ffe5117ad85474934fb721e13c9abc8c100
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-21 14:24:13 +0100

    clean up lonas screen

--- src/components/LoansScreen/LoansScreen.jsx
+++ src/components/LoansScreen/LoansScreen.jsx
@@ -9,30 +9,6 @@ export default function LoansScreen() {
     <div className="wrapper">
       <div className="box four-tiles-section">
         <FourTiles />
-        {/* <div className="two-small-sections">
-          <SmallSection
-            title={"Personal Loans"}
-            amount={50000}
-            Icon={PersonalLoansIcon}
-          />
-          <SmallSection
-            title={"Corporate Loans"}
-            amount={100000}
-            Icon={CorporateLoansIcon}
-          />
-        </div>
-        <div className="box two-small-sections">
-          <SmallSection
-            title={"Business Loans"}
-            amount={500000}
-            Icon={BusinessLoansIcon}
-          />
-          <SmallSection
-            title={"Custom Loans"}
-            amount={100000}
-            Icon={CustomLoansIcon}
-          />
-        </div> */}
       </div>
       <div className="box active-loans-overview-sections">
         <div className="top-banner">
